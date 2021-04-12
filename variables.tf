d825093b884d60ddb4b0e042ef391b33cb84229a
lokpavan03 <80965262+lokpavan03@users.noreply.github.com>
2021-04-12 13:13:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,21 @@
- variable "token" {
-  type    = string
-  #default = "eab04c08-63fc-4336-909a-e1b7f7d8ca1e"
-}
-
 variable "subscription_id" {
   type    = string
-  #default = "eab04c08-63fc-4336-909a-e1b7f7d8ca1e"
+  default = ""
 }
 
 variable "client_id" {
   type    = string
-  #default = "24cad02a-ae8d-4ef2-93c8-c7a8afb31cf6"
+  default = ""
 }
 
 variable "client_secret" {
   type    = string
-  #default = "I1iU0RphD3FOgQz~7JjCK8o1s_RW93~~NB"
+  default = ""
 }
 
 variable "tenant_id" {
   type    = string
-  #default = "7f3f7804-5327-4427-ba97-e16ad406dd43"
+  default = ""
 }
 
 variable "resource_group_name" {
