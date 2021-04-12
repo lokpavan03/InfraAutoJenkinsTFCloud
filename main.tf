99150a5bdf953f5dde73986a06155392d30d00ad
lokpavan03 <80965262+lokpavan03@users.noreply.github.com>
2021-04-12 11:44:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ terraform {
 #Configure the Microsoft Azure Provider and here logging to Azure with the Service Principal
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = "eab04c08-63fc-4336-909a-e1b7f7d8ca1e"
+  client_id       = "24cad02a-ae8d-4ef2-93c8-c7a8afb31cf6"
+  client_secret   = "-~_pDh0o0sF649FAfzTyyd~gl2DL4_USvA"
+  tenant_id       = "7f3f7804-5327-4427-ba97-e16ad406dd43"
 }
 
 #Create a Resource Group in Azure using Service Principal
